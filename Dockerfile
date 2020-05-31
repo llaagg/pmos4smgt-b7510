9b58da9ac8175dabcd49c859e0fc260d69b164b8
Pawel Galezowski <pawel.galezowski@tfbank.pl>
2020-05-31 14:38:58 +0200

    working first 2 steps

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,30 @@
 FROM alpine
 
+ARG USER=pmosuser
+
 RUN apk update && apk upgrade && \
-    apk add --no-cache bash git openssh sudo
+    apk add --no-cache bash git openssh sudo 
 
 RUN apk add python3 coreutils procps openssl
 
-# go with new user
-RUN adduser  -D -u 1000 pmosuser
-USER pmosuser
-WORKDIR /home/pmosuser
+#usefull for obtaining boot.img
+#RUN apk --update-cache --repository http://dl-3.alpinelinux.org/alpine/edge/testing/ add android-tools
+#RUN echo 'root:docker' | chpasswd
+RUN adduser -D $USER \
+        && echo "$USER ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/$USER \
+        && chmod 0440 /etc/sudoers.d/$USER
 
+#bugfix warkaround (https://github.com/sudo-project/sudo/issues/42)
+RUN echo "Set disable_coredump false" >> /etc/sudo.conf
 
-
+USER $USER
+WORKDIR /home/$USER
 RUN pip3 install --user pmbootstrap
 
+ADD config.cfg  /home/pmosuser/config.cfg
+RUN cat config.cfg | .local/bin/pmbootstrap init
 
-#RUN git clone https://gitlab.com/postmarketOS/pmbootstrap.git
-#RUN python3 "$PWD/pmbootstrap/setup.py" install
+RUN sed -i -e '/pkgver/ s/=.*/=3.0.0/' /home/pmosuser/.local/var/pmbootstrap/cache_git/pmaports/device/testing/linux-samsung-gtb7510/APKBUILD
+RUN .local/bin/pmbootstrap checksum samung-gtb7510
 
+RUN cat /home/pmosuser/.local/var/pmbootstrap/log.txt
--- build.ps1
+++ build.ps1
@@ -1,1 +1,23 @@
 docker build . -t pmos:dev -f 'Dockerfile'
+
+# mount needs that: --privileged
+docker run --privileged --rm -it  pmos:dev
+
+#docker run -ti -d --privileged -v /dev/bus/usb:/dev/bus/usb pmos:dev
+
+# podobny do https://www.mgsm.pl/pl/katalog/samsung/gts7580/
+# Root information http://www.bernaerts-nicolas.fr/android/311-samsung-galaxy-pro-gt-b7510-root
+
+#compatible SAMSUNG GALAXY MODELS: GT-S5570, GT-S5570B, GT-S5570I, GT-S5570L, GT-S5571, GT-S5578 (Chinese-Mini), 
+# GT-S5660, GT-S5660M, GT-S5660L, GT-S5660V, SHW-M290K (Korean-Gio), GT-S5670, GT-S5670B, GT-S5670L, GT-S5830, GT-S5830B, 
+# GT-S5830C, GT-S5830D, GT-S5830F(La Fleur), GT-S5830G, GT-S5830i, GT-S5830L, GT-S5820, GT-S5830M, GT-S5830T, GT-S5830V, GT-S5830Z, 
+# GT-S5838(Chinese-Ace), GT-S5839i, GT-S6800(Africa-Ace Advance), GT-S6802, GT-S6802B, GT-S6802L (new), GT-B5330, GT-B7510, GT-B7510B,
+# GT-B7510L, GT-B7800, GT-B5510, GT-B5510B, GT-B5510L, GT-B5512, GT-B5512B, GT-S6102, GT-S6102B, GT-S5300, GT-S5300B, GT-S5300L (new), 
+# GT-S5302, GT-S5302B (new), GT-S5302L (new), GT-S5360, GT-S5360B, GT-S5360L, GT-S5360T, GT-S5363, GT-S5367 (new), GT-S5368 (new), GT-S5369,
+# GT-S5690, GT-S5690i, GT-S5690L, GT-S5690M, GT-S5690R, GT-I8150, GT-I8150B, GT-I5510, GT-I5510B, GT-I5510L, GT-I5510M, GT-I5510T, SCH-i110 (new), 
+# SCH-i509, SCH-i559, SCH-i589(Ace Duos -Chinese/India), SGH-T499, SGH-T499Y, SGH-T499V, SGH-T499W, SGH-T589, SGH-T589R, SGH-T589W, SGH-I837, SGH-I857.
+
+# [09:30:35] *** pmaport generated: /home/pmosuser/.local/var/pmbootstrap/cache_git/pmaports/device/testing/device-samsung-gtb7510
+# [09:30:35] *** pmaport generated: /home/pmosuser/.local/var/pmbootstrap/cache_git/pmaports/device/testing/linux-samsung-gtb7510
+
+# pmbootstrap bootimg_analyze
